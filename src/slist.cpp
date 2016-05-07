6d98fd639b2b3076082d971cff69f8572c10f874
Abhishek Kondaveeti <abhishek.kondaveeti@gmail.com>
2016-05-07 10:56:48 +0530

    Prac: Support for finding permutations of polindromes
    Finding polindrome permutations in a string added
    
    Signed-off-by: Abhishek Kondaveeti <abhishek.kondaveeti@gmail.com>

--- src/slist.cpp
+++ src/slist.cpp
@@ -27,6 +27,20 @@ slist::slist(int data)
 	m_list->next = NULL;
 }
 
+slist::~slist()
+{
+	struct singlenode *list = m_list;
+	struct singlenode *delnode = NULL;
+
+	while (list->next != NULL) {
+		delnode = list->next;
+		delete list;
+		list = delnode;
+	}
+
+	delete list;
+	m_list = NULL;
+}
 int slist::add(int data)
 {
 	int  rc = 0;
